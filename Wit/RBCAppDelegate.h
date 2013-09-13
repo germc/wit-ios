536416650d7af410ec1105857644b261f7a6de21
Rachel Brindle <you@subluminal.net>
2013-09-12 17:43:04 -0700

    Initial Commit

---
+++ Wit/RBCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RBCAppDelegate.h
+//  Wit
+//
+//  Created by Rachel Brindle on 9/12/13.
+//  Copyright (c) 2013 Rachel Brindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RBCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wit
+//
+//  Created by Rachel Brindle on 9/12/13.
+//  Copyright (c) 2013 Rachel Brindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RBCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RBCAppDelegate class]));
+    }
+}
